68c08971630dd81439a5bf14d6296bc39ce19705
Mike Hamilton <mike.hamilton@neudesic.com>
2016-04-19 09:22:23 -0700

    working UGLY site with images

--- NWVDNUGSite/Controllers/HomeController.cs
+++ NWVDNUGSite/Controllers/HomeController.cs
@@ -3,36 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using NWVDNUG.Entities;
-using NWVDNUG.Services;
-using NWVDNUG.ViewModels;
 
 namespace NWVDNUG.Controllers
 {
 	public class HomeController : Controller
 	{
-		private MeetingsService _meetingsService = new MeetingsService();
-
 		public ActionResult Index()
 		{
 			var model = BuildHomePageModel();
 			return View(model);
 		}
 
-		private HomePageViewModel BuildHomePageModel()
+		private ActionResult BuildHomePageModel()
 		{
-			var nextMeeting = _meetingsService.GetNextMeeting() ?? new Meeting
-				{
-					IsArchived = false,
-					Title = "No Meeting Info Found",
-					SpeakerName = "",
-					Location = "Foothills Recreation Center, 5600 W Union Hills Ave, Glendale, AZ, 85308",
-				};
-
-			return new HomePageViewModel
-					   {
-						   NextMeetingInfo = nextMeeting
-					   };
+		    return View();
 		}
 
 		public ActionResult About()
